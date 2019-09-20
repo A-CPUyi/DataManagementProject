9d4e0bc9c875ca3f3cf93ca489e702ea6313c8aa
CPU-Yi <lanyi0810@gmail.com>
2019-09-20 18:51:44 -0400

    dev: sort business categories

--- MySQLscript/import_business.sql
+++ MySQLscript/import_business.sql
@@ -18,7 +18,7 @@ set unique_checks = 0;
 #create table (keys disabled
 #ignored: "review_count","is_open","attributes"{json object},"categories"{string},"hours"{json object}}
 create table if not exists Business(
-	id char(50),
+	id char(22),
     business_name varchar(100),
     Address varchar(200),
     City text, #there is one extreamly long city name
@@ -27,20 +27,24 @@ create table if not exists Business(
     latitude float,
     longitude float,
     stars float,
+    categories varchar(600),
     primary key (id)
 );
 alter table Business disable keys;
 
 #parse json into column
 insert into Business (id, business_name, Address, City, State, postal_code,
-	latitude, longitude, stars)
-	select jsonData->'$.business_id' , jsonData->'$.name', jsonData->'$.address',
+	latitude, longitude, stars, categories)
+	select replace(jsonData->'$.business_id', '"',''), replace(jsonData->'$.name', '"',''), jsonData->'$.address',
 		jsonData->'$.city', jsonData->'$.state', jsonData->'$.postal_code',
-		jsonData->'$.latitude', jsonData->'$.longitude', jsonData->'$.stars'
+		jsonData->'$.latitude', jsonData->'$.longitude', jsonData->'$.stars',
+        replace(jsonData->'$.categories','"','')
 	from json_temp;
+    
+
 
 #drop origin json table
-drop table json_temp;
+#drop table json_temp;
 ALTER TABLE Business ENABLE KEYS;
 SET UNIQUE_CHECKS = 1;
 SET FOREIGN_KEY_CHECKS = 1;
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,82 @@
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
+
 /**
  * Main
  */
 public class Main {
+    static Connection connect = null;
+
     public static void main(String[] args) {
+
+        // todo: should be user input
+        String userName = "test";
+        String password = "123456";
+        String hostIP = "68.180.87.215";
+        String port = "3306";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection(String.format("jdbc:mysql://%s:%s/", hostIP, port), userName,
+                    password);
+            if (!connect.isClosed()) {
+                connect.setSchema("project1_main");
+                java.sql.Statement stmt = connect.createStatement();
+                ResultSet res = stmt.executeQuery("Select * from project1_main.business");
+
+                HashMap<String, Integer> attrbute_count_map = new HashMap<>();
+                // todo: print results
+                while (res.next()) {
+                    String rawStrCategories = res.getString("categories");
+                    String[] items = rawStrCategories.split(", ");
+                    for (String str : items) {
+                        if (attrbute_count_map.putIfAbsent(str, 1) == null)
+                            ;
+                        else {
+                            attrbute_count_map.compute(str, (k, v) -> v = v.intValue() + 1);
+                        }
+                    }
+                }
+                List<Entry<String, Integer>> resultList = new ArrayList<>(attrbute_count_map.entrySet());
+                // .forEach((e) -> resultList.add(e));
+                Collections.sort(resultList, (i, j) -> {
+                    return j.getValue() - i.getValue();
+                });
+
+                try {
+                    FileWriter fw = new FileWriter("F:\\codeRepo\\java\\BigDataProj1\\output.txt");
+                    resultList.forEach((i) -> {
+                        try {
+                            fw.write(i.toString() + "\n");
+                        } catch (IOException e1) {
+                            // TODO Auto-generated catch block
+                            e1.printStackTrace();
+                        }
+                    });
+                    fw.close();    
+                   }
+                catch(Exception e){System.out.println(e);}
+                System.out.println("Success...");  
+
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         System.out.println("hello world");
     }
 }
