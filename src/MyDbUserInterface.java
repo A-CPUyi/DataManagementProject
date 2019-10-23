529fe769b4c34e87fbed29de3549f893ee858f5f
CPU-Yi <lanyi0810@gmail.com>
2019-10-23 19:06:29 -0400

    dev: switch display type

--- src/MyDbUserInterface.java
+++ src/MyDbUserInterface.java
@@ -5,12 +5,24 @@ import java.sql.SQLException;
  * Main
  */
 public class MyDbUserInterface {
+    enum DisplayType{
+        Console, GUI, Default;
+    }
+
     static Connection dB_connection;
+    static final DisplayType DISPLAY_TYPE = DisplayType.GUI;
     private static void init() throws SQLException {
         //setSchema dose not work, use this
         dB_connection.setCatalog("project1_main");
-        Display display = new ConsoleDisplay();
-        display.init();
+        switch (DISPLAY_TYPE) {
+            case GUI:
+                new GuiDisplay().init();
+                break;
+            case Console:
+            default:
+                new ConsoleDisplay().init();
+                break;
+        }
     }
     public static void main(String[] args) throws SQLException {
         dB_connection = Utility.getConnection();
